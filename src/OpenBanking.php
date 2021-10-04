d28b448f488de1def913047d5e46f2b492162a34
Guilherme Calabria Filho <guiga86@gmail.com>
2021-10-04 16:15:14 -0300

    Update OpenBanking.php

--- src/OpenBanking.php
+++ src/OpenBanking.php
@@ -19,198 +19,281 @@ class OpenBanking {
 	private $banco;
 	private $appdevkey;
 	private $clientid;
-	private $clientscret;
+	private $clientsecret;
    private $sandbox = false;
 
-	// Armazena a última token processada pelo método obterToken()
+	// Armazena a Ãºltima token processada pelo mÃ©todo obterToken()
 	private $tokenEmCache;
 
-	// Tempo em segundos válido da token gerada pelo BB
-	private $ttl_token = 600;
-	// Porcentagem tolerável antes de tentar renovar a token (0 a 100). Se ultrapassar, tente renová-la automaticamente. // 0 (zero) -> sempre renova
-	// 100 -> tenta usá-la até o final do tempo
-	private $porcentagemtoleravel_ttl_token = 80;
+	// Tempo em segundos vÃ¡lido da token gerada pelo BB
+	private static $ttl_token = 600;
+	// Porcentagem tolerÃ¡vel antes de tentar renovar a token (0 a 100). Se ultrapassar, tente renovÃ¡-la automaticamente. // 0 (zero) -> sempre renova
+	// 100 -> tenta usÃ¡-la atÃ© o final do tempo
+	private static $porcentagemtoleravel_ttl_token = 80;
 	// Tempo limite para obter resposta de 20 segundos
 	private $timeout = 20;
 
-   private $tokenDir='';
+	private $tiporequisicao = '';
 
 	/**
   	 * Construtor do Consumidor de WebService do BB
   	 *
-  	 * @param array $params Parâmetros iniciais para construção do objeto
-  	 * @throws Exception Quando o banco não é suportado
+  	 * @param array $params ParÃ¢metros iniciais para construÃ§Ã£o do objeto
+  	 * @throws Exception Quando o banco nÃ£o Ã© suportado
   	 */
 	public function __construct(array $params)
 	{
       if ( isset($params['banco']) ) {
          if ( $params['banco']!='001' ){
-            throw new \Exception('Banco ('.$params['banco'].') não possui suporte para OpenBanking');
+            throw new \Exception('Banco ('.$params['banco'].') nÃ£o possui suporte para OpenBanking');
          }
 
          $this->banco = $params['banco'];
       }
-      if ( isset($params['clientid']) ) $this->client_id = $params['clientid'];
-      if ( isset($params['clientscret']) ) $this->client_scret = $params['clientscret'];
+      if ( isset($params['clientid']) ) $this->clientid = trim($params['clientid']);
+      if ( isset($params['clientsecret']) ) $this->clientsecret = trim($params['clientsecret']);
       if ( isset($params['sandbox']) && \is_bool($params['sandbox']) ) $this->sandbox = $params['sandbox'];
       if ( isset($params['appdevkey']) ) $this->appdevkey = $params['appdevkey'];
-      if ( isset($params['tokendir']) ){
-         if ( !\is_dir($params['tokendir']) ){
-            throw new \Exception('Diretório ('.$params['tokendir'].') para token inválido');
-         }
-         $this->tokendir = $params['tokendir'];
-      }
+      if ( isset($params['tiporequisicao']) ) $this->tiporequisicao = $params['tiporequisicao'];
 	}
 
 	/**
-	 * Inicia as configurações do Curl útil para
-	 * realizar as requisições de token e registro de boleto
-	 * @returns resource Curl pré-configurado
+	 * Inicia as configuraÃ§Ãµes do Curl Ãºtil para
+	 * realizar as requisiÃ§Ãµes de token e registro de boleto
+    * @param bollean $autorizacao Se for para preparar o CURL para solicitar token
+	 * @returns resource Curl prÃ©-configurado
+    * @throws Exception Quando houver
 	 */
 	private function prepararCurl($autorizacao=false)
 	{
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-			//CURLOPT_BINARYTRANSFER => true,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_SSL_VERIFYPEER => false,
-			CURLOPT_SSL_VERIFYHOST => 0,
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_POST => true,
-			CURLOPT_TIMEOUT => $this->_timeout,
-			CURLOPT_MAXREDIRS => 3
-		));
-
-      if ( $autorizacao===false ){
-
+      $escopo = '';
+      /*Se for solicitar token exibir escopo */
+      if ( $autorizacao!==false && $this->tiporequisicao=='' ){
+         throw new \Exception('Escopo nÃ£o configurado para preparar CURL ');
       }
-      else {
+      elseif ( $autorizacao!==false ){
+         switch($this->tiporequisicao){
+            case 'cobrancas.requisicao':$escopo = 'cobrancas.boletos-requisicao';
+               break;
+            case 'cobrancas.info':
+            default:$escopo = 'cobrancas.boletos-info';
+         }
+      }
+
+      try{
+         $curl = curl_init();
          curl_setopt_array($curl, array(
-            CURLOPT_POSTFIELDS => 'grant_type=client_credentials',
-            CURLOPT_HTTPHEADER => array(
-               'Authorization: Basic ' . base64_encode($this->clientid . ':' . $this->clientscret),
-               'Cache-Control: no-cache'
+            //CURLOPT_BINARYTRANSFER => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_POST => true,
+            CURLOPT_TIMEOUT => $this->timeout,
+            CURLOPT_MAXREDIRS => 3
+         ));
+
+         if ( $autorizacao===false ){
+            curl_setopt_array($curl, array(
+               CURLOPT_HTTPHEADER =>  array(
+                  //'Authorization: Bearer '.$this->obterToken(false),
+                  'Authorization: Bearer '.$this->obterToken(true),
+                  //'X-Application-Key: '.$this->appdevkey,
+                  'Content-Type: application/json'
                )
             ));
+         }
+         else {
+            curl_setopt_array($curl, array(
+               CURLOPT_URL => $this->getURL(true),
+               CURLOPT_POSTFIELDS => 'grant_type=client_credentials'.($escopo!=''?'&scope='.$escopo:''),
+               CURLOPT_HTTPHEADER => array(
+                  'Authorization: Basic ' . base64_encode($this->clientid . ':' . $this->clientsecret),
+                  'Cache-Control: no-cache'
+               )
+            ));
+         }
+         return $curl;
+      }catch(Exception $e){
+         throw new \Exception('Erro ao iniciar CURL:'.$e->getMessage());
       }
-      return $curl;
 	}
 
 	/**
 	 * Retorna a determinada URL
-	 * @returns boll Curl pré-configurado
-    * @throws Exception Quando dados inválidos poara URL
+	 * @returns boll Curl prÃ©-configurado
+    * @throws Exception Quando dados invÃ¡lidos poara URL
 	 */
 	private function getURL($autorizacao=false)
 	{
       switch($this->banco=='001')
       {
-         case '001':if ($autorizacao===false) return $this->sandbox ? 'https://oauth.sandbox.bb.com.br/oauth/token':'';
-                  else return $this->sandbox ? 'https://api.sandbox.bb.com.br/cobrancas/v2':'https://api.bb.com.br/cobrancas/v2';
+         case '001':if ($autorizacao===true) return $this->sandbox ? 'https://oauth.sandbox.bb.com.br/oauth/token':'';
+                  else return $this->sandbox ? 'https://api.hm.bb.com.br/cobrancas/v2':'https://api.bb.com.br/cobrancas/v2';
             break;
       }
-      throw new \Exception('Dados inválidos para URL');
+      throw new \Exception('Dados invÃ¡lidos para URL');
 	}
 
 	/**
-  	 * Inicia as configurações do Curl útil para realizar as requisições
-  	 * @param bool $naousarcache		Especifica se o programador aceita ou não obter uma token já salva em cache
-  	 * @returns object|bool Objeto, caso o token foi recebido com êxito, ou false, caso contrário
+  	 * Inicia as configuraÃ§Ãµes do Curl Ãºtil para realizar as requisiÃ§Ãµes
+  	 * @param bool $naousarcache		Especifica se o programador aceita ou nÃ£o obter uma token jÃ¡ salva em cache
+  	 * @returns object|bool Objeto, caso o token foi recebido com Ãªxito, ou false, caso contrÃ¡rio
+    * @throws Exception Quando houver
   	 */
 	public function obterToken($naousarcache = true)
 	{
+      try{
 			if ($this->tokenEmCache && !$naousarcache) return $this->tokenEmCache;
 
-         if ( $this->tokenDir!='' )
-         {
-            // Define o caminho para o arquivo de cache
-            $caminhodoarquivodecache = $this->tokenDir.DIRECTORY_SEPARATOR .'token.txt';
+         $curl = $this->prepararCurl(true);
 
-            if (!$naousarcache)
-            {
-               // Se o arquivo existir, retorna o timestamp da última modificação. Se não, retorna "false"
-               $timedamodificacao = @filemtime($caminhodoarquivodecache);
+         $resposta = curl_exec($curl);
 
-               // Testa se o arquivo existe e se o seu conteúdo (token) foi modificado dentro do tempo tolerável
-               if ($timedamodificacao && $timedamodificacao + self::$ttl_token * self::$porcentagemtoleravel_ttl_token / 100 > time())
-               {
-                  // Tenta abrir o arquivo para leitura e escrita
-                  $arquivo = @fopen($caminhodoarquivodecache, 'c+');
-
-                  // Se conseguir-se abrir o arquivo...
-                  if ($arquivo)
-                  {
-                     // trava-o para escrita enquanto os dados são lidos
-                     flock($arquivo, LOCK_SH);
-
-                     // Lê o conteúdo do arquivo
-                     $dados = '';
-                     do
-                        $dados .= fread($arquivo, 1024);
-                     while (!feof($arquivo));
-
-                     fclose($arquivo);
-
-                     // Retorna apenas a token salva no arquivo
-                     return $this->tokenEmCache = $dados;
-                  }
-               }
-            }
-         }
-
-         try{
-
-            $curl = $this->prepararCurl();
-            curl_setopt_array($curl, array(
-               CURLOPT_URL => $this->getURL(true)
-            ));
-         }catch(Exception $e){
-            throw new \Exception('Erro ao obter token:'.$e->getMessage());
-         }
-
-			$resposta = curl_exec($curl);
-			curl_close($curl);
+         curl_close($curl);
 
 			// Recebe os dados do WebService no formato JSON.
 			// Realiza o parse da resposta e retorna.
 			// Caso seja um valor vazio ou fora do formato, retorna false.
 			$resultado = json_decode($resposta);
-\var_dump($resultado);
+
 			// Se o valor salvo em "$resultado" for um objeto e se existir o atributo "access_token" nele...
 			if ($resultado)
 			{
 				if (isset($resultado->access_token))
 				{
-               if ( $this->tokenDir='' )
-               {
-                  // Armazena token em cache apenas se a porcentagem tolerável sobre o tempo da token for superior a 0%
-                  if (self::$porcentagemtoleravel_ttl_token > 0)
-                  {
-                     // Tenta abrir o arquivo para leitura e escrita
-                     $arquivo = @fopen($caminhodoarquivodecache, 'c+');
-
-                     // Se conseguir-se abrir o arquivo...
-                     if ($arquivo) {
-                        // trava-o para leitura e escrita
-                        flock($arquivo, LOCK_EX);
-
-                        // apaga todo o seu conteúdo
-                        ftruncate($arquivo, 0);
-
-                        // escreve a token no arquivo
-                        fwrite($arquivo, $resultado->access_token);
-
-                        fclose($arquivo);
-                     }
-                  }
-               }
-
                $this->tokenEmCache = $resultado->access_token;
 					return $this->tokenEmCache;
 				}
 			}
+         return false;
 
-		return false;
+      }catch(Exception $e){
+         throw new \Exception('Erro ao obter token:'.$e->getMessage());
+      }
 	}
 
+	/**
+  	 * Regitra o determinado boleto no banco
+  	 * @param array $dados  Dados do para o boleto
+  	 * @returns array|bool Objeto, caso o registro foi recebido com Ãªxito retorna os dados, ou false, caso contrÃ¡rio
+    * @throws Exception Quando houver
+  	 */
+   public function registrarBoleto(array $dados)
+   {
+      try{
+            $campoPossivel = array('numeroConvenio','numeroCarteira','numeroVariacaoCarteira','codigoModalidade','dataEmissao','dataVencimento','valorOriginal','valorAbatimento','quantidadeDiasProtesto','quantidadeDiasNegativacao','orgaoNegativador',
+               'indicadorAceiteTituloVencido','numeroDiasLimiteRecebimento','codigoAceite','codigoTipoTitulo','descricaoTipoTitulo','indicadorPermissaoRecebimentoParcial','numeroTituloBeneficiario','campoUtilizacaoBeneficiario','numeroTituloCliente',
+               'mensagemBloquetoOcorrencia','desconto'=>array('tipo','dataExpiracao','porcentagem','valor'),'segundoDesconto'=>array('dataExpiracao','porcentagem','valor'),'terceiroDesconto'=>array('dataExpiracao','porcentagem','valor'),
+               'jurosMora'=>array('tipo','porcentagem','valor'),'multa'=>array('tipo','data','porcentagem','valor'),'pagador'=>array('tipoInscricao','numeroInscricao','nome','endereco','cep','cidade','bairro','uf','telefone'),
+               'beneficiarioFinal'=>array('tipoInscricao','numeroInscricao','nome'),'indicadorPix');
+
+         $dadosBoleto = array();
+         foreach($campoPossivel as $key=>$nome)
+         {
+            if ( is_array($nome) )
+            {
+               if ( !is_array($dados[$key]) ) continue;
+               $dadosBoleto[$key] = array();
+
+               foreach($nome as $nomeinterno)
+               {
+                  if ( isset($dados[$key][$nomeinterno]) ) $dadosBoleto[$key][$nomeinterno] = $dados[$key][$nomeinterno];
+               }
+            }
+            elseif ( isset($dados[$nome]) ) $dadosBoleto[$nome] = $dados[$nome];
+         }
+         $curl = $this->prepararCurl();
+
+         $encodedData = json_encode($dadosBoleto);
+         //echo 'A'.$encodedData,"\n";
+         $url = $this->getURL();
+         $url = $this->getURL().'/boletos';
+         $url = $this->getURL().'/?gw-dev-app-key='.$this->appdevkey;
+         $url = $this->getURL().'/boletos?gw-dev-app-key='.$this->appdevkey.'&indicadorSituacao=A&agenciaBeneficiario=452&contaBeneficiario=123873&codigoEstadoTituloCobranca&dataInicioVencimento=14.09.2021&dataFimVencimento=14.09.2021';
+         $url = $this->getURL().'/boletos/00031285570000003051/?gw-dev-app-key='.$this->appdevkey.'';
+         $url = $this->getURL().'/boletos?gw-dev-app-key='.$this->appdevkey;
+         //echo 'URL',"\n",$teste,"\n";
+         curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            //CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $encodedData
+         ));
+
+			$resposta = curl_exec($curl);
+			curl_close($curl);
+
+         if ( $resposta )
+         {
+            // Recebe os dados do WebService no formato JSON.
+            // Realiza o parse da resposta e retorna.
+            // Caso seja um valor vazio ou fora do formato, retorna false.
+            $resultado = json_decode($resposta,true);
+            if ( isset($resultado['erros']) ){
+               throw new \Exception(var_export($resultado['erros'],true));
+            }
+            elseif ( isset($resultado['error']) ){
+               throw new \Exception($resultado['message']);
+            }
+            return $resultado;
+         }
+         else {
+            throw new \Exception('Erro ao registrar boleto nÃ£o foi possÃ­vel conectar ao banco');
+         }
+
+      }catch(Exception $e){
+         throw new \Exception('Erro ao registrar boleto:'.$e->getMessage());
+      }
+   }
+	/**
+  	 * Busca os dados do determinado boleto
+  	 * @param array $busca  Dados para busca
+  	 * @returns array|bool Objeto dados do(s) boleto(s)
+    * @throws Exception Quando houver
+  	 */
+   public function getBoleto(array $dados)
+   {
+      try{
+
+         $curl = $this->prepararCurl();
+
+         $url = $this->getURL().'/boletos';
+         $parametros = '?gw-dev-app-key='.$this->appdevkey;
+         if ( isset($dados['id']) ) $url .= '/'.$dados['id'];
+         if ( isset($dados['numeroConvenio']) ) $parametros .= '&numeroConvenio='.$dados['numeroConvenio'];
+
+         $url .= $parametros;
+
+         curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+         ));
+
+			$resposta = curl_exec($curl);
+			curl_close($curl);
+
+         if ( $resposta )
+         {
+            // Recebe os dados do WebService no formato JSON.
+            // Realiza o parse da resposta e retorna.
+            // Caso seja um valor vazio ou fora do formato, retorna false.
+            $resultado = json_decode($resposta,true);
+            if ( isset($resultado['erros']) ){
+               throw new \Exception(var_export($resultado['erros'],true));
+            }
+            elseif ( isset($resultado['error']) ){
+               throw new \Exception($resultado['message']);
+            }
+            return $resultado;
+         }
+         else {
+            throw new \Exception('Erro ao consultar boleto');
+         }
+
+      }catch(Exception $e){
+         \print_r($e->getMessage());
+         throw new \Exception('Erro ao consultar boleto:'.$e->getMessage());
+      }
+   }
 }
